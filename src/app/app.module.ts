bbaf417bc05a136887462bf8ac205598d567af1e
Shermam Xavier Miranda <sxmiranda@latam.stefanini.com>
2019-05-16 14:23:37 -0300

    Ajuste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,9 @@ import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MasterPageComponent } from "./app-master-page/master-page.component";
-import { ManagerAddEditComponent } from './modules/trouble-shooting/manager-add-edit/manager-add-edit.component';
-import { ManagerListComponent } from './modules/trouble-shooting/manager-list/manager-list.component';
-import { ManagerFilterComponent } from './modules/trouble-shooting/manager-filter/manager-filter.component';
 
 @NgModule({
-  declarations: [AppComponent, MasterPageComponent, ManagerAddEditComponent, ManagerListComponent, ManagerFilterComponent],
+  declarations: [AppComponent, MasterPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
