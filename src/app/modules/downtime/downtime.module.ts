a26c1d06365b10dec3b28cf60773996f92b5894a
Shermam Xavier Miranda <sxmiranda@latam.stefanini.com>
2019-05-16 11:45:33 -0300

    added routes

--- src/app/modules/downtime/downtime.module.ts
+++ src/app/modules/downtime/downtime.module.ts
@@ -5,15 +5,21 @@ import { DowntimeRoutes } from "./downtime.routing";
 
 // Created Components
 import { EventAddEditComponent } from "./events/event-add-edit/event-add-edit.component";
-import { EventListComponent } from './events/event-list/event-list.component';
-import { EventFilterComponent } from './events/event-filter/event-filter.component';
-import { ReasonsFilterComponent } from './reasons-filter/reasons-filter.component';
-import { ReasonFilterComponent } from './reason-filter/reason-filter.component';
-import { ReasonListComponent } from './reasons/reason-list/reason-list.component';
-import { ReasonAddEditComponent } from './reasons/reason-add-edit/reason-add-edit.component';
+import { EventListComponent } from "./events/event-list/event-list.component";
+import { EventFilterComponent } from "./events/event-filter/event-filter.component";
+import { ReasonFilterComponent } from "./reasons/reason-filter/reason-filter.component";
+import { ReasonListComponent } from "./reasons/reason-list/reason-list.component";
+import { ReasonAddEditComponent } from "./reasons/reason-add-edit/reason-add-edit.component";
 
 @NgModule({
-  declarations: [EventAddEditComponent, EventListComponent, EventFilterComponent, ReasonsFilterComponent, ReasonFilterComponent, ReasonListComponent, ReasonAddEditComponent],
+  declarations: [
+    EventAddEditComponent,
+    EventListComponent,
+    EventFilterComponent,
+    ReasonFilterComponent,
+    ReasonListComponent,
+    ReasonAddEditComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(DowntimeRoutes)]
 })
 export class DowntimeModule {}
--- src/app/modules/downtime/downtime.routing.ts
+++ src/app/modules/downtime/downtime.routing.ts
@@ -2,6 +2,9 @@ import { Routes } from "@angular/router";
 import { EventAddEditComponent } from "./events/event-add-edit/event-add-edit.component";
 import { EventListComponent } from "./events/event-list/event-list.component";
 import { EventFilterComponent } from "./events/event-filter/event-filter.component";
+import { ReasonFilterComponent } from "./reasons/reason-filter/reason-filter.component";
+import { ReasonListComponent } from "./reasons/reason-list/reason-list.component";
+import { ReasonAddEditComponent } from "./reasons/reason-add-edit/reason-add-edit.component";
 
 export const baseRoutes: Routes = [
   {
@@ -10,7 +13,7 @@ export const baseRoutes: Routes = [
   },
   {
     path: "reasons",
-    redirectTo: "reasons-list"
+    redirectTo: "reason-list"
   }
 ];
 
@@ -33,6 +36,22 @@ export const DowntimeRoutes: Routes = baseRoutes.concat([
       {
         path: "event-filter",
         component: EventFilterComponent
+      },
+      {
+        path: "reason-edit/:id",
+        component: ReasonAddEditComponent
+      },
+      {
+        path: "reason-add",
+        component: ReasonAddEditComponent
+      },
+      {
+        path: "reason-list",
+        component: ReasonListComponent
+      },
+      {
+        path: "reason-filter",
+        component: ReasonFilterComponent
       },
       {
         path: "",
