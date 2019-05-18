7bfb16c04e9ffc026427244d09e049d7e33983ef
Shermam Miranda <shermam.miranda@hotmail.com>
2019-05-18 17:01:25 -0300

    included toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MasterPageComponent } from "./app-master-page/master-page.component";
+import { MatToolbarModule, MatButtonModule } from "@angular/material";
 
 @NgModule({
   declarations: [AppComponent, MasterPageComponent],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register("ngsw-worker.js", {
