b7a5ab7fa49f79da37986c8c73dec8b0a753b6a9
Shermam Miranda <shermam.miranda@hotmail.com>
2019-05-22 10:33:57 -0300

    ajustes de configuracao do identity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: "",
     component: MasterPageComponent,
-    // canActivate: [AuthGuardService],
+    canActivate: [AuthGuardService],
     children: [
       {
         path: "downtime",
